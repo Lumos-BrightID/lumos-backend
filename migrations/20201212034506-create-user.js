e7483c1adb2b78edaf4b3e928915c78dcb597fae
walton mohsen <waltonmohsen@gmail.com>
2020-12-16 06:51:25 +0330

    update user migration, added unique option

--- migrations/20201212034506-create-user.js
+++ migrations/20201212034506-create-user.js
@@ -14,30 +14,37 @@ module.exports = {
             },
             telegram: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             twitter: {
                 type: Sequelize.STRING,
+                unique: true
                 allowNull: true,
             },
             facebook: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             instagram: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             mobile: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             email: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             erc20Address: {
                 type: Sequelize.STRING,
+                unique: true,
                 allowNull: true,
             },
             createdAt: {
