e8d06e420cf09f02e9545f1d973ca596ccc54252
walton mohsen <waltonmohsen@gmail.com>
2020-12-16 06:50:45 +0330

    complete check method and added middleware for validation

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models/index')
+const {body, checkSchema, validationResult} = require('express-validator');
+
 
 // simple middleware for checking requested body
-// router.use(function timeLog(req, res, next) {
-//     if (req.body.social == null) {
-//         res.status(406).json({
-//             status: 'not acceptable',
-//             message: 'social is required'
-//         })
-//     }
-//     next()
-// })
+router.use(checkSchema({
+    username: {
+        options: {
+            min: 3,
+            max: 40
+        }
+    },
+    type: {
+        in: ['body'],
+        matches: {
+            options: [/\b(?:telegram|twitter|instagram|phone|email|facebook|erc20Address)\b/],
+            errorMessage: "Invalid role"
+        }
+    }
+}), (req, res, next) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    next()
+})
 
 
-// router.post('/profile', function (req, res, next) {
-//     const token = req.cookies.login
-//     // check brightid and uniqness of the contextId
-// })
-
-router.get('/check', function (req, res) {
+/**
+ * check the user profile on the social media
+ */
+router.get('/check', async function (req, res) {
     const {username, type} = req.body
 
+    try {
+        const exists = await db.user.findOne({
+            where: {
+                [type]: username
+            },
+        })
+        // check the query result
+        if (!exists) {
+            res.status(404).json({
+                'message': 'user not found or not verified yet!'
+            })
+        } else {
+            res.status(200).json({
+                'message': 'user is verified'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
