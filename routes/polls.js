69ab61160e710ad10f372a41d73e39bdd9e9285e
walton mohsen <waltonmohsen@gmail.com>
2020-12-17 21:35:14 +0330

    complete poll method

--- routes/polls.js
+++ routes/polls.js
@@ -1,16 +1,49 @@
-const {check} = require("express-validator")
+const {validationResult} = require('express-validator');
+const {check,checkSchema} = require("express-validator")
 const express = require('express');
 const router = express.Router();
 const db = require('../models/index')
 
-
-router.post('/', [
+/**
+ * middleware that validate request body
+ */
+router.use([
+    checkSchema({
+       type:{
+           in: ['body'],
+           matches: {
+               options: [/\b(?:telegram|twitter|instagram|phone|email|facebook|erc20Address)\b/],
+               errorMessage: "Invalid type"
+           }
+       }
+    }),
     check('data').isArray(),
-    check('data.*').isString()
-], async function (req, res) {
-    const data = req.body.data
+    check('data.*').isString(),
+],function(req, res,next){
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    next()
+})
+
+/**
+ * return a list of verified and unverified users
+ */
+router.post('/',  async function (req, res) {
+    const {type, data} = req.body
+    let list = []
+    for (const username of data) {
+        let user = await db.user.findOne({
+            where: {
+                [type]:username
+            }
+        })
+        user ? list.push({ user: username,verified:1 }) : list.push({ user: username,verified:0 })
+    }
     res.json({
-        data
+        data: list
     })
 })
 
