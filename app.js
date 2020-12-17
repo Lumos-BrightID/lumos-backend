d75912fa0a379b84afdf2da1b5dfe0dce0d4d312
walton mohsen <waltonmohsen@gmail.com>
2020-12-17 21:34:52 +0330

    .

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 40 // limit each IP to 100 requests per windowMs
+    max: 100 // limit each IP to 100 requests per windowMs
 })
 
 app.use('/api/', limiter)
